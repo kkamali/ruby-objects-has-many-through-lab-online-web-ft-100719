3d20f3fec0b6415481fa118ecb11c06d37981721
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-16 22:02:34 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select{|appointment| appointment.patient == select}
+    Appointment.all.select{|appointment| appointment.patient == self }
   end 
   
   def doctors 
